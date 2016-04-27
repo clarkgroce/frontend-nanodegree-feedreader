e3a08bb9c38b6e62508091a2df1ce87975047717
Clark Groce <cgroce@cgroce-ltm.internal.salesforce.com>
2016-04-27 17:43:40 -0400

    added Initial Entries test suite and test to check if entry added to feed container when loadFeed called.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,13 +84,30 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+         */ 
+
+        beforeEach(function(done) {
+            //load the feed
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('should load a feed', function(done) {
+            // '.entry' elements descended from '.feed'
+            var entries = $('.feed').has('.entry');
+            // Number of entries should not be 0
+            expect(entries.length).not.toBe(0);
+            done();
+        });
+    });     
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
