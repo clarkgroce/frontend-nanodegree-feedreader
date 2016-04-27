610cddf6665e737ce59eb69a54127c6c8ff08fbd
Clark Groce <cgroce@cgroce-ltm.internal.salesforce.com>
2016-04-27 17:53:49 -0400

    added New Feed Selection test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -109,10 +109,17 @@ $(function() {
         });
     });     
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+            /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+         
+
+    });
+
+
 }());
